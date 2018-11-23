8ba238e2ee4545322db6f8c2d6299723901dd9c3
marlarsoe <nawbeauty21@gmail.com>
2018-11-23 15:32:07 +0630

    type your commit message here

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :comments
+  get 'pages/info'
+  get "pages/info"
+	root to: redirect('/ideas')
+  resources :ideas
+  resources "entries"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
